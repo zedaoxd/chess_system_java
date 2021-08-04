a430fe5ec94b23f472ebaf2c87a5ca6351ec985b
Bruno Lessa <brunolessa@live.com>
2021-08-04 14:27:32 -0300

    att

--- chess_system/src/application/UI.java
+++ chess_system/src/application/UI.java
@@ -5,11 +5,13 @@ import chess.ChessPiece;
 public class UI {
     
     public static void printBoard(ChessPiece[][] pieces){
+        System.out.println("  a b c d e f g h");
         for(int i=0; i<pieces.length; i++){
             System.out.print((8-i) + " ");
             for(int j=0; j<pieces[0].length; j++){
                 printPiece(pieces[i][j]);
             }
+            System.out.print(8-i);
             System.out.println("");
         }
         System.out.println("  a b c d e f g h");
