b93bc675fef6696b421d332e76c7f796d6cb7048
Bruno Lessa <brunolessa@live.com>
2021-08-04 13:01:25 -0300

    First class: Position

--- chess_system/src/application/main.java
+++ chess_system/src/application/main.java
@@ -1,7 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Helo World!");
+        
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos);
     }
 }
