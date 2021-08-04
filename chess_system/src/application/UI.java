4f75901bef43fc3ee41102e485a8496b258001d4
Bruno Lessa <brunolessa@live.com>
2021-08-04 16:31:59 -0300

    up

--- chess_system/src/application/UI.java
+++ chess_system/src/application/UI.java
@@ -40,16 +40,16 @@ public class UI {
     }
     
     public static void printBoard(ChessPiece[][] pieces){
-        System.out.println("  a b c d e f g h");
+        System.out.println(ANSI_RED + ANSI_YELLOW_BACKGROUND + "  a b c d e f g h  " + ANSI_RESET);
         for(int i=0; i<pieces.length; i++){
-            System.out.print((8-i) + " ");
+            System.out.print(ANSI_RED + ANSI_YELLOW_BACKGROUND + (8-i) + ANSI_RESET+ " ");
             for(int j=0; j<pieces[0].length; j++){
                 printPiece(pieces[i][j]);
             }
-            System.out.print(8-i);
+            System.out.print(ANSI_RED + ANSI_YELLOW_BACKGROUND + (8-i) + ANSI_RESET);
             System.out.println("");
         }
-        System.out.println("  a b c d e f g h");
+        System.out.println(ANSI_RED + ANSI_YELLOW_BACKGROUND + "  a b c d e f g h  " + ANSI_RESET);
     }
 
     private static void printPiece(ChessPiece piece){
